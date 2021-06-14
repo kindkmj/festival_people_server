5ab42fd966619c6527a7c848c84d698eec54aa2b
minjin <kindcode@gmail.com>
2021-06-14 21:01:44 +0900

    1
    1

---
+++ feastival/src/main/java/com/feastival/project/FeastivalApplication.java
@@ -0,0 +1,13 @@
+package com.feastival.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeastivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeastivalApplication.class, args);
+	}
+
+}
---
+++ feastival/src/test/java/com/feastival/project/FeastivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feastival.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeastivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
